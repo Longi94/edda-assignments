b525a49b3652abeb9b5ae87b71e4583afa0fdd46
Daniel Galea <danielgalea1995@gmail.com>
2018-02-27 16:14:19 +0100

    Updated E1 and report

--- assign2/exercise1.r
+++ assign2/exercise1.r
@@ -8,17 +8,30 @@ t_stat = median(data)
 B = 1000
 tstar = numeric(B)
 n = length(data)
-for(i in 1:B){
- xstar = rexp(n,c(0.01,0.1))
- tstar[i] = median(xstar)
+
+seqconst = seq(0.01,0.1,0.001)
+constindex = 1
+parr = numeric(length(seqconst))
+
+for(j in seqconst){
+  for(i in 1:B){
+    xstar = rexp(n,j)
+    tstar[i] = median(xstar)
+  }
+  
+  pl = sum(tstar<t_stat)/B
+  pr=sum(tstar>t_stat)/B
+  p = 2*min(pl,pr)
+  parr[constindex] = p
+  constindex = constindex+1
 }
 
-hist(tstar, prob=T)
-pl = sum(tstar<t_stat)/B
-pr=sum(tstar>t_stat)/B
-p = 2*min(pl,pr)
-p
+seqconst[parr>0.05]
+plot(parr)
 
 #p is always below 0.05 and therefore it does not stem from the exponential distribution. 
 #Furthermore, the histogram resembles more of a bell-shaped curve rather than an exponential one
 
+
+boxplot(tstar)
+qqnorm(tstar); qqline(tstar)

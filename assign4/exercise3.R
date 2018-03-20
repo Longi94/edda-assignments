03545e39e5a5f6d9d20ec82974d7c5549fd6be80
Daniel Galea <danielgalea1995@gmail.com>
2018-03-21 00:14:20 +0100

    Finished E3 (both script and in report)

--- assign4/exercise3.R
+++ assign4/exercise3.R
@@ -1,25 +1,22 @@
-par(mfrow=c(2,4))
+par(mfrow=c(2,3))
 data = read.table("africa.txt",header=TRUE)
 attach(data)
 #1
 
-sample1 = rpois(20,5)
-plot(sample1);#lines(lowess(sample1))
-qqnorm(sample1);qqline(sample1)
-sample2 = rpois(30,7.5)
-plot(sample2);#lines(lowess(sample2))
-qqnorm(sample2);qqline(sample2)
-sample3 = rpois(40,10)
-plot(sample3);#lines(lowess(sample3))
-qqnorm(sample3);qqline(sample3)
-sample4 = rpois(50,12.5)
-plot(sample4);#lines(lowess(sample4))
-qqnorm(sample4);qqline(sample4)
-#not sure what to say here, but as the lambda value increases (2nd paramater) the distribution looks more normal (as expected)
+n = c(50,150,300)
+lambda = c(1,10,50)
+for(i in n){
+  for(j in lambda){
+    data_generated = rpois(i,j)
+    boxplot(data_generated, main=c("n",i,"lambda",j))
+    qqnorm(data_generated, main=c("n",i,"lambda",j));qqline(data_generated)
+  }
+}
+
 
 
 #2
-#this part is more theoretical, I need to look into it.
+#this part is more theoretical.
 
 #3
 africaglm=glm(miltcoup~oligarchy+pollib+parties+pctvote+popn+size+numelec+numregim,family=poisson,data=data) 
@@ -34,10 +31,12 @@ summary(glm(miltcoup~oligarchy+pollib+parties+pctvote,family=poisson,data=data))
 summary(glm(miltcoup~oligarchy+pollib+parties,family=poisson,data=data))
 #Following this approach, the new model is: miltcoup = 0.251377 + 0.092622*oligarchy - 0.574103*pollib + 0.22059*parites + error
 #5
+par(mfrow=c(1,3))
 model = glm(miltcoup~oligarchy+pollib+parties,family=poisson,data=data)
 #diagonistic plots
-plot(fitted(model),residuals(model));lines(lowess(fitted(model),residuals(model)))
-plot(log(fitted(model)),residuals(model));lines(lowess(log(fitted(model)),residuals(model)))
-plot(fitted(africaglm), residuals(africaglm));lines(lowess(fitted(africaglm),residuals(africaglm)))
-plot(log(fitted(africaglm)),residuals(africaglm));lines(lowess(log(fitted(africaglm)),residuals(africaglm)))
-#I cannot see any pattern
+plot(fitted(model),residuals(model))
+plot(log(fitted(model)),residuals(model))
+plot(log(fitted(model)),residuals(model, type="response"))
+plot(fitted(africaglm), residuals(africaglm))
+plot(log(fitted(africaglm)),residuals(africaglm))
+plot(log(fitted(africaglm)),residuals(africaglm, type="response"))
